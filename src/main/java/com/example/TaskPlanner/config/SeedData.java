0a3686fcfdd987a326de2701abe7972ab78cf81a
franklin efe <frankyx41@yahoo.com>
2024-01-20 17:50:44 +0100

    SeedData commandLineRunner

---
+++ src/main/java/com/example/TaskPlanner/config/SeedData.java
@@ -0,0 +1,38 @@
+package com.example.TaskPlanner.config;
+
+import com.example.TaskPlanner.models.StatusType;
+import com.example.TaskPlanner.models.Task;
+import com.example.TaskPlanner.services.TaskService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class SeedData implements CommandLineRunner {
+
+    final TaskService taskService;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        List<Task> tasks = taskService.getAll();
+        if (tasks.isEmpty()){
+            Task task1 = Task.builder()
+                    .description("learn SQL")
+                    .status(StatusType.BACKLOG)
+                    .build();
+            Task task2 = Task.builder()
+                    .description("Practise Spring-Boot")
+                    .status(StatusType.DOING)
+                    .build();
+            Task task3 = Task.builder()
+                    .description("Blog-App")
+                    .status(StatusType.DONE)
+                    .build();
+        }
+
+    }
+}
